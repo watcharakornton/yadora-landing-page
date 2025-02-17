b4c653afccd16752ee065a95e430114c74bf047b
Watcharakorn Jantarasangaram (SI) <watcharakorn.j@Watcharakorns-MacBook-Air.local>
2025-02-17 18:40:24 +0700

    Develop Dashboard scene

--- src/components/sections/Dashboard.jsx
+++ src/components/sections/Dashboard.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import dashboardImage from "../../assets/dashboard.webp"
 
-const Dashboard = () => {
+function Dashboard() {
     return (
-        <div>Dashboard</div>
-    )
+        <section className="relative">
+            <div className="bottom-0 -z-10 absolute bg-primary-500 w-full h-1/2" />
+            <div className="justify-items-center grid m-auto px-4 sm:px-8 md:px-16 xl:px-24 py-8 md:py-16 max-w-[90rem]">
+                <img src={dashboardImage} alt="Dashboard" className="block w-full" />
+            </div>
+        </section>
+    );
 }
 
-export default Dashboard
+export default Dashboard;
